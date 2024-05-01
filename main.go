dae5165c2a4f537d5c3fb47dfe70431f3fce34f0
root <root@DESKTOP-CA21UEV>
2024-04-30 21:56:05 -0300

    Criação das structs de cada entidade

--- main.go
+++ main.go
@@ -1,10 +1,22 @@
 package main
 
 type UserAccount struct {
+	ID        int64
 	FirstName string
 	LastName  string
-	Cpf       string
+	CPF       string
 	Balance   float64
+	CreatedAt string
+	UpdatedAt string
+	DeletedAt string
+}
+
+type UserTransaction struct {
+	ID        int64
+	SenderID  int64
+	ReciverID int64
+	Amount    float64
+	CreatedAt string
 }
 
 func main() {
